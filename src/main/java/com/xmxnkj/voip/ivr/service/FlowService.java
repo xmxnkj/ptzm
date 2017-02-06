60bbeea8bffb4c71c72520fd4fd6619f4d0eb77e
chenwu0 <chenwu0@aliyun.com>
2017-02-06 09:50:01 +0800

    模板之类

---
+++ src/main/java/com/xmxnkj/voip/ivr/service/FlowService.java
@@ -0,0 +1,9 @@
+package com.xmszit.voip.ivr.service;
+
+import com.hsit.common.service.BusinessBaseService;
+import com.xmszit.voip.ivr.entity.Flow;
+import com.xmszit.voip.ivr.entity.query.FlowQuery;
+
+public interface FlowService extends BusinessBaseService<Flow, FlowQuery> {
+
+}
---
+++ src/main/java/com/xmxnkj/voip/ivr/service/FlowTemplateService.java
@@ -0,0 +1,9 @@
+package com.xmszit.voip.ivr.service;
+
+import com.hsit.common.service.BusinessBaseService;
+import com.xmszit.voip.ivr.entity.FlowTemplate;
+import com.xmszit.voip.ivr.entity.query.FlowTemplateQuery;
+
+public interface FlowTemplateService extends BusinessBaseService<FlowTemplate, FlowTemplateQuery>{
+
+}
